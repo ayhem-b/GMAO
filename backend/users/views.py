347ed9bffb8fec251cc7a44f1d9fe60abe88fb6e
belkhamsa Ayhem <belkhamsaayhem@bizerte.r-iset.tn>
2025-03-16 17:38:14 +0100

    authentication finished
    i created the login & logout methods

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
-# Create your views here.  
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout
+
+# Create your views here.
 def admin(request):
-    return render(request,"users/admin.html")
- 
+    return render(request, "users/admin.html")
+
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
@@ -12,14 +14,23 @@ def register(request):
             return redirect("users/login.html")
     else:
         form = UserCreationForm()
-    return render(request,"users/register.html",{"form":form}) 
+    return render(request, "users/register.html", {"form": form})
 
-def login(request):
+def user_login(request):  # I Renamed the function to avoid conflict with built-in login function
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            return redirect("users:admin")
-
+            user = form.get_user()
+            auth_login(request, user)  # I Used auth_login instead of login 
+            if user.is_superuser:
+                return redirect("users:admin")
+            else:
+                return redirect("users:register")         
     else:
         form = AuthenticationForm()
-    return render(request,"users/login.html",{"form":form})
+    return render(request, "users/login.html", {"form": form})
+
+def user_logout(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("users:login")
