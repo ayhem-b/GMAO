67e64399e0d205cc71004d5c11ce753b077e81f0
belkhamsa Ayhem <ayabajay@gmail.com>
2025-03-24 00:29:31 +0100

    updating some changes

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'Gmao',
-        'USER': 'postgres',
+        'USER': 'admin',
         'PASSWORD': '0000',
         'HOST': '127.0.0.1',
         'PORT': '5432',
